1cadd02a6a3736e7cd5991983689c20695ce4f13
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-07-02 11:15:01 +0200

    Called our selected User within the user.component.html file
    "as above" also added some missing semicolons

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users'; 
 // der doppelte Punkt beim aufrufen des dummy user arrays sorgt dafür das wir in der projektstruktur eine eben höher danach suchen
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)  
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);  
 // die .floor Funktion gibt einen Integer gleich groß oder kleiner als die Anzahl an Usern in unserem Array wieder
 // die .random Funktion ruft nun eine zufällige Position in unseren Array auf
 
@@ -14,6 +14,6 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]     
+  selectedUser = DUMMY_USERS[randomIndex]; 
   // die oben geschriebene Konstante wird hier nun anstelle einer statischen Zahl verwendet und macht die auswahl> dynamisch/zufällig
 }
