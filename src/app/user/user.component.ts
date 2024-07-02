0c9ece5d01d4a2f1cc5ec1259646a774c697eb8f
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-07-02 10:36:54 +0200

    Added Constant randomIndex to our user.component.ts file
    We imported the Dummy User Array and created a constant that gives us a random position from that array by looking up an integer as big or smaller that the length of our array
    we than call this selected User within our exported class that we can access from our template

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users'; 
+// der doppelte Punkt beim aufrufen des dummy user arrays sorgt dafür das wir in der projektstruktur eine eben höher danach suchen
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)  
+// die .floor Funktion gibt einen Integer gleich groß oder kleiner als die Anzahl an Usern in unserem Array wieder
+// die .random Funktion ruft nun eine zufällige Position in unseren Array auf
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,5 +14,6 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex]     
+  // die oben geschriebene Konstante wird hier nun anstelle einer statischen Zahl verwendet und macht die auswahl> dynamisch/zufällig
 }
