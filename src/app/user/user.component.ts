58d055d2a5970d1f7b93548c4cb57b03eb672631
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-08-02 09:49:40 +0200

    Changed to a modern EmitterFunction
    though most projects will have the old @Output function we will try this one for the course

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
-import { combineLatest } from 'rxjs';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,7 +10,8 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  @Output() select = new EventEmitter();
+  //@Output() select = new EventEmitter();
+  select = output<string>();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
