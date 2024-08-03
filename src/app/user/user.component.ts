06ba0b3a048372a8ec6c84391bbc658059b32ec3
SteffenRothenberg <126148715+SteffenRothenberg@users.noreply.github.com>
2024-08-03 12:59:57 +0200

    reusing the @Output function
    earlier version of this part of the code is pretty modern and not used yet by any big companies therefore we adjust and go back to the @Output on select funciton.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,8 +10,7 @@ export class UserComponent {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
-  //@Output() select = new EventEmitter();
-  select = output<string>();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
